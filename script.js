0333539ff4b1eb930c67d343cc1a99e7dff009a6
jpfagundes <jpsafagundes@hotmail.com>
2023-04-01 02:43:35 -0300

    sprint 02 and 03 mobile

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const menu = document.querySelector('.menu-mobile');
+const menuDesktop = document.querySelector('.card .menu-desktop');
+const divider = document.querySelector('.divider')
+const menuOpenMobile = document.querySelector('.btn-menu .menu-open');
+const menuClosedMobile = document.querySelector('.btn-menu .menu-closed');
+
+
+function clickMenu() {
+  menu.classList.toggle('element-hidden');
+  menuDesktop.classList.toggle('element-hidden');
+  menuOpenMobile.classList.toggle('element-hidden');
+  menuClosedMobile.classList.toggle('element-hidden');
+}
+
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
